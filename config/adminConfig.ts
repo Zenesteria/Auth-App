427382e8c344e2c09d796a72bbd7a2a8c8e1a231
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-10-10 11:25:58 +0100

    credentials fix

--- config/adminConfig.ts
+++ config/adminConfig.ts
@@ -16,9 +16,22 @@ const serviceCredentials = process.env.NODE_ENV == 'production' ? JSON.stringify
     client_x509_cert_url:process.env.NEXT_CLIENT_CERT
 }) : require('../serviceAccountKey.json');
 
+// const serviceCredentials  = JSON.stringify({
+//         type:process.env.NEXT_TYPE,
+//         project_id:process.env.NEXT_PROJECT_ID,
+//         private_key_id:process.env.NEXT_PRIVATE_KEY_ID,
+//         private_key:process.env.NEXT_PRIVATE_KEY,
+//         client_email:process.env.NEXT_CLIENT_EMAIL,
+//         client_id:process.env.NEXT_CLIENT_ID,
+//         auth_url:process.env.NEXT_AUTH_URL,
+//         token_url:process.env.NEXT_TOKEN_URL,
+//         auth_provider_x509_cert_url:process.env.NEXT_AUTH_PROVIDER,
+//         client_x509_cert_url:process.env.NEXT_CLIENT_CERT
+// })
+
 
 const adminConfigOpts = {
-    credential:credential.cert(serviceCredentials)
+    credential:process.env.NODE_ENV == 'production' ? JSON.parse(serviceCredentials) : credential.cert(serviceCredentials)
 }
 
 const admin = !firebase.apps.length ? initializeApp(adminConfigOpts, 'admin') : firebase.app('admin')
