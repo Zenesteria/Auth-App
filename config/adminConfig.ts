16514dc20cd165610418ff594dc3b00a320b3c6c
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-10-10 19:27:01 +0100

    credentials bug fix

--- config/adminConfig.ts
+++ config/adminConfig.ts
@@ -1,16 +1,21 @@
 import { initializeApp} from "firebase-admin/app";
-import { credential } from "firebase-admin";
 import {getAuth} from 'firebase-admin/auth'
 import * as firebase from 'firebase-admin'
 
-const serviceCredentials =  require('../serviceAccountKey.json')
+import serviceCredentials from "./serviceCredentials";
 
 
-// console.log(credential.cert(serviceCredentials))
+
 
 const adminConfigOpts = {
-    credential:credential.cert(serviceCredentials)
+    credential:firebase.credential.cert({
+        projectId:process.env.NEXT_PROJECT_ID,
+        clientEmail:process.env.NEXT_CLIENT_EMAIL,
+        privateKey:process.env.NEXT_PRIVATE_KEY?.replace(/\\n/g, '\n')
+    })
 }
+
+console.log(adminConfigOpts)
 
 const admin = !firebase.apps.length ? initializeApp(adminConfigOpts) : firebase.app()
 
